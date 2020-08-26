a1bac7e7282e7e7a6d0313e20617703a608a8ac9
Rohit Bhosale <rohitb13@kpit.com>
2020-08-26 12:26:06 +0530

    First commit

---
+++ src/sonarqube/demosonar.java
@@ -0,0 +1,20 @@
+package sonarqube;
+
+public class demosonar {
+
+	public void test()
+	{
+		
+	}
+	
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello How are you ?");
+	}
+
+}
